bb2000d07f1b448cce43611580fc62bd2113b039
josuakohler <josua.kohler05@gmail.com>
2024-08-05 08:56:31 +0200

    searchroute component

--- reisePlanerVite/src/App.vue
+++ reisePlanerVite/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue';
-</script>s
+import SearchRoute from './components/SearchRoute.vue';
+</script>
 
 <template>
 
+  <SearchRoute from="Sargans" to="Zürich"></SearchRoute>
   <HelloWorld departure="December 17, 1995 03:24:00" arrival="December 17, 1995 08:24:00" platForm="2" stationName="Zürich"></HelloWorld>
+  <RoutePlayList></RoutePlayList>
 </template>
--- reisePlanerVite/src/components/SearchRoute.vue
+++ reisePlanerVite/src/components/SearchRoute.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
-import { defineComponent } from 'vue';
+  defineProps<{
+    from: string;
+    to: string;
 
-defineComponent({
-    data() {
-        return {
-           
-            routeList: [] as String[],
-            from: '' as string,
-            to: '' as string,
-        };
-    },
-    methods: {
-        async searchRoutes() {
-            const response = await fetch(`http://transport.opendata.ch/v1/connections?from=${this.from}&to=${this.to}`);
-            const data = await response.json();
-            this.routeList = data.connections;
-        },
+  }>();
 
 
-    }
+
+// async searchRoutes() {
+//           const response = await fetch(`http://transport.opendata.ch/v1/connections?from=${ from }&to=${ to }`);
+//           const data = await response.json();
+//           this.routeList = data.connections;
+//       };
 
 
-    
-})
 </script>
+<template>
+  <div class="search">
+    <div class="from-to">
+      <div class="from">
+        <label for="from">Von:</label>
+        <input type="text" id="from" placeholder="Sargans" />
+      </div>
+      <div class="to">
+        <label for="to">Nach:</label>
+        <input
+          type="text"
+          id="to"
+          placeholder="Chur"
+        />
+      </div>
+    </div>
+    <button>Suche nach Routen</button>
+    <p>Suche nach Routen Von: {{ from }} Nach: {{ to }}</p>
+  </div>
+</template>
